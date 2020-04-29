895d17da3018076c0fb02fab03ca138e5cf134e6
Lee, Hosoo <Hosoo.Lee@arithnea.de>
2020-04-29 15:18:18 +0200

    03-Selection Functions

--- myScript.js
+++ myScript.js
@@ -1,3 +1,8 @@
-var headerID=d3.select('#headerID')
-console.log(headerID.text());
-headerID.text("D3 Setup has been compoleted");
+// 02-D3.js Setup
+// var headerID=d3.select('#headerID')
+// console.log(headerID.text());
+// headerID.text("D3 Setup has been compoleted");
+
+// 03- Selection Functions
+var selection=d3.selectAll("div");
+selection.filter(function(d,i) {return true;}).select("p").text("Updated");
